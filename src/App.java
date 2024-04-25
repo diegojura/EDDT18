a5f0dcb8e0e6d3c493ccea288f1d9fbbfc56c334
diegojura <154268497+diegojura@users.noreply.github.com>
2024-04-25 17:21:06 +0200

    release2

--- src/App.java
+++ src/App.java
@@ -3,6 +3,6 @@ public class App {
         System.out.println("Hello, hotfix inicio!");
         System.out.println("Hello, develop 2!");
         System.out.println("Hello, feature 4!");
-        System.out.println("Hello, release!");
+        System.out.println("Hello, release 2!");
     }
 }
